cb3333f9ecdd36e62970015971d60c35da15eba6
akalandiya <100804445+akalandiya@users.noreply.github.com>
2022-03-21 23:19:26 +0300

    Добавление тестов

--- test/tests.cpp
+++ test/tests.cpp
@@ -2,3 +2,63 @@
 
 #include <gtest/gtest.h>
 #include "Automata.h"
+
+TEST(taskA, test1)
+{
+  Automata a = Automata();
+  a.on();
+  int result = a.getState();
+  EXPECT_EQ(WAIT, result);
+}
+
+TEST(taskA, test2)
+{
+  Automata a = Automata();
+  a.on();
+  a.coin(100);
+  int result = a.getState();
+  EXPECT_EQ(ACCEPT, result);
+}
+
+TEST(taskA, test3)
+{
+  Automata a = Automata();
+  a.on();
+  a.coin(100);
+  a.choice(1);
+  int result = a.getState();
+  EXPECT_EQ(ACCEPT, result);
+}
+
+TEST(taskA, test4)
+{
+  Automata a = Automata();
+  a.on();
+  a.coin(200);
+  a.choice(1);
+  int result = a.getState();
+  EXPECT_EQ(COOK, result);
+}
+
+TEST(taskA, test4)
+{
+  Automata a = Automata();
+  a.on();
+  a.coin(200);
+  a.choice(1);
+  a.finish();
+  int result = a.cancel();
+  EXPECT_EQ(80, result);
+}
+
+TEST(taskA, test4)
+{
+  Automata a = Automata();
+  a.on();
+  a.coin(200);
+  a.choice(1);
+  a.finish();
+  a.off();
+  int result = a.getState();
+  EXPECT_EQ(OFF, result);
+}
