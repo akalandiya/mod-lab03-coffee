cc6e902a3cca0242564d869e25555fceb956b270
akalandiya <100804445+akalandiya@users.noreply.github.com>
2022-03-21 23:07:07 +0300

    Добавление сценария использования

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "Automata.h"
+
+using std::string;
+using std::cout;
+
+int main()
+{
+    Automata a;
+    int b;
+    a.on();
+    cout << "automat vkluchen!\n";
+    a.coin(200);
+    cout << "dengi zaneseni!\n";
+    a.choice(1);
+    cout << "super! gotovim!\n";
+    a.finish();
+    b=a.cancel();
+    cout << "sdacha! "<<b;
+    a.off();
+    cout << "\nautomat vikluchen!\n";
+}
